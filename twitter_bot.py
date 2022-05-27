161997b034c12ce21d483a215c3517cc7bc8b8e3
Maksim Sukhanovm <kakxow@gmail.com>
2022-05-27 14:45:32 +0300

    ready for launch

--- twitter_bot.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-from constants import url_post_tweet, twitter_headers
-
-
-def send_tweet(text: str) -> tuple[str, str]:
-    r = requests.post(url_post_tweet, json=text, headers=twitter_headers)
-    response_data = r.json()
-    return response_data["data"]["id"], response_data["data"]["text"]
